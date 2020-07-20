e589eb0ed67e8e1f0c62f519a345bdfe3be04d97
Matteo Carpi <matteocarpi94@gmail.com>
2020-07-20 18:24:55 +0200

    feat: can mutate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import head from './images/head.png';
 import body from './images/body.png';
 import butt from './images/butt.png';
@@ -6,13 +6,22 @@ import styles from './styles/App.module.scss';
 import classnames from 'classnames';
 
 function App() {
+  const [mutated, setMutated] = useState(false);
+  const [mutating, setMutating] = useState(false);
   return (
     <div className={styles.App}>
       <div className={styles.drosophila}>
-        <img src={head} className={classnames(styles.head, styles.bodyPart, styles.mutateHead)}></img>
-        <img src={body} className={classnames(styles.body, styles.bodyPart, styles.mutateBody)}></img>
-        <img src={butt} className={classnames(styles.butt, styles.bodyPart, styles.mutateButt)}></img>
+        <img src={head} className={classnames(styles.head, styles.bodyPart, mutated && styles.mutateHead)}></img>
+        <img src={body} className={classnames(styles.body, styles.bodyPart, mutated && styles.mutateBody)}></img>
+        <img src={butt} className={classnames(styles.butt, styles.bodyPart, mutated && styles.mutateButt)}></img>
       </div>
+
+      <button className={classnames(styles.mutationButtonon, mutating && styles.hide)} onClick={() => {
+        setMutating(true);
+        setTimeout(() => setMutating(false), 6000);
+        setMutated(!mutated);
+      }}>{!mutated ? 'Mutate me!' : 'Get me back to normal!'}</button>
+      {console.log('mutating: ', mutating)};
     </div>
   );
 }
