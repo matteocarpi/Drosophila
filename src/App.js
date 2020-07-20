e660a8fcb88d2c676f1fdc039727f33411d3881f
Matteo Carpi <matteocarpi94@gmail.com>
2020-07-20 17:36:48 +0200

    feat: put images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import head from './images/head.png';
+import body from './images/body.png';
+import butt from './images/butt.png';
+import styles from './styles/App.module.scss';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <div className={styles.drosophila}>
+        <img src={head} className={styles.head}></img>
+        <img src={body} className={styles.body}></img>
+        <img src={butt} className={styles.butt}></img>
+      </div>
     </div>
   );
 }
