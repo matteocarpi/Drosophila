4339398808bd07fced1aefe3b2adebac2d0771d8
Matteo Carpi <matteocarpi94@gmail.com>
2020-07-20 18:06:55 +0200

    feat: drosophila animations

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import head from './images/head.png';
 import body from './images/body.png';
 import butt from './images/butt.png';
 import styles from './styles/App.module.scss';
-
+import classnames from 'classnames';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <div className={styles.drosophila}>
-        <img src={head} className={styles.head}></img>
-        <img src={body} className={styles.body}></img>
-        <img src={butt} className={styles.butt}></img>
+        <img src={head} className={classnames(styles.head, styles.bodyPart, styles.mutateHead)}></img>
+        <img src={body} className={classnames(styles.body, styles.bodyPart, styles.mutateBody)}></img>
+        <img src={butt} className={classnames(styles.butt, styles.bodyPart, styles.mutateButt)}></img>
       </div>
     </div>
   );
